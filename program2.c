cb6a0f3b7ecc9c189fc8cc571d3eb4d1c85b2133
Szawelski <szawelski@gmail.com>
2017-10-19 17:41:37 +0200

    drugi program

---
+++ program2.c
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+
+
+
+
+int main() 
+{ 
+    putchar('v');  /* wypisz pojedynczy znak */
+    putchar('\n'); /* znak konca linii */
+    puts("wypisz string / ciag znakow"); /* putchar('v'); wypisuje jeden znak */  
+
+    puts("\nAla ma kota,\na kot ma Ale");
+
+    return 0; 
+}
+
