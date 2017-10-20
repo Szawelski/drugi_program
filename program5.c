179398ee76ad9d46f585eb139255f7491f9dd8f1
Szawelski <szawelski@gmail.com>
2017-10-20 15:38:24 +0200

    kalkulator

--- program5.c
+++ program5.c
@@ -17,13 +17,25 @@ int dziel(int a, int b){
     printf("Wynik dzielenia: %i i reszta %i\n", a / b,a % b);
     return a / b;
 }
+int licz(int a, int b, int wybor)
+{
+
+}
 int main(void)
 {
     int a, b;
     char wybor;
+    int czy_dalej == 1;
+
+    while(czy_dalej == 1){
     printf("Podaj wartosc a: "); scanf("%i", &a);
     printf("Podaj wartosc b: "); scanf("%i", &b);
     printf("Podaj dzialanie: "); scanf(" %c", &wybor);
+    licz(a, b, wybor);
+    printf("Czy dalej chcesz liczyc? (1 - tak / 0 - nie");
+    scanf("%d", &czy_dalej);
+    }
+    return 0;
     if(wybor == '*'){
     mnoz(a, b);
     }else if(wybor == '+'){
