ffdd62ee7a5ba94830a6caa5ca0bc6e20c6a9ba4
Szawelski <szawelski@gmail.com>
2017-10-20 11:02:59 +0200

    kolejny program

--- program2.c
+++ program2.c
@@ -3,13 +3,16 @@
 #include <stdio.h>
 
 
-
+/* int wartosc calkowita*/
 
 int main() 
 { 
     putchar('v');  /* wypisz pojedynczy znak */
     putchar('\n'); /* znak konca linii */
-    puts("wypisz string / ciag znakow"); /* putchar('v'); wypisuje jeden znak */  
+    putchar('\t'); /* tabulator */
+    putchar('\r'); /* powrot karetki do nowej linii */
+    putchar('\\'); /* wypisuje \ */
+    puts("wypisz string / ciag znakow"); /* automatycznie przechodzi do nowej linii, putchar('v'); wypisuje jeden znak */  
 
     puts("\nAla ma kota,\na kot ma Ale");
 
